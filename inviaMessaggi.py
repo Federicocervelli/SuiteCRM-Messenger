e1e94947b059afc4b6bf7811d311d8989631fb29
Federico Cervelli <federicocervelli01@gmail.com>
2023-04-30 12:44:48 +0200

    aggiunto start point

--- inviaMessaggi.py
+++ inviaMessaggi.py
@@ -1,28 +1,36 @@
 import invio
 import ripartizione
 import sys
+import logging
 
 #Valori di default
 verbose = False
 path = "/var/log/inviomessaggi.log"
 
+
+
 def main():
     #Analisi delle casistiche della chiamata del comando
     analisiComando()
+    #Inizializzazione log
+    configurazioneLog()
     #Accesso all'API
     autenticazione()
-    
-        
+    #test logging in invio
+    invio.test()
+          
 def analisiComando():
     args = sys.argv[1:]
     global path, verbose #Cambio lo scope a globale
-    if len(args) == 2 and args[1] == "-v":
-        path = args[0]
+    if len(args) == 2 and args[0] == "-v":
+        path = args[1]
         verbose = True
     elif len(args) == 1 and args[0] == "-v":
         verbose = True
     elif len(args) == 1:
         path = args[0]
+    elif len(args) == 0:
+        return
     else:
         stampaErroreSintassi()
 
@@ -31,4 +39,19 @@ def stampaErroreSintassi():
     print("Utilizzo corretto: python3 inviaMessaggi.py [-v] [path]")
     
 def autenticazione():
-    print()
+    logging.debug("Conclusa funzione autenticazione")
+    
+def configurazioneLog():
+    
+    if(verbose):
+        tipolog = logging.DEBUG
+    else:
+        tipolog = logging.INFO
+        
+    logging.basicConfig(filename=path, level=tipolog,
+                    format='[%(asctime)s] [%(levelname)s] %(message)s',
+                    datefmt='%Y/%m/%d %H:%M:%S')
+    logging.info("Programma inizializzato con i seguenti parametri: " + str(sys.argv[1:]))
+    
+if __name__ == "__main__":
+    main()
---
+++ invio.py
@@ -0,0 +1,5 @@
+import logging
+logger = logging.getLogger(__name__)
+
+def test():
+    logger.debug("Eseguita funzione test in invio")
